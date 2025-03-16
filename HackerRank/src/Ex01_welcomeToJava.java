3ad3d916cf6b28275de4e49e9fb8cfbe40938c00
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-03-16 03:54:24 -0300

    add Ex01

---
+++ HackerRank/src/Ex01_welcomeToJava.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.*;
+
+public class Ex01_welcomeToJava {
+    public static void main (String[] args) {
+        String str = "Hello ";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Adicione uma palavra: ");
+        String input = scanner.nextLine();
+
+        System.out.print("Adicione outra palavra: ");
+        String inputTwo = scanner.nextLine();
+
+        System.out.println(str + input.substring(0, 1).toUpperCase() + input.substring(1));
+        System.out.print(str + inputTwo.substring(0, 1).toUpperCase() + inputTwo.substring(1));
+
+        scanner.close();
+    }
+}
